ab019e2c44aa1f3e9de1ee27113b2b8c03ba4352
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-07-02 22:36:32 +0600

    susbscription schema

---
+++ models/subscription.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const SubscriptionSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Subscription name is required"],
+    trim: true,
+    minLength: 2,
+    maxLength: 100,
+  },
+  price: {
+    type: Number,
+    required: [true, "Subscription price is required"],
+    min: [0, "Price must be greater than 0"],
+  },
+});
