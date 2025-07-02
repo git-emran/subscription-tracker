d584a0d99254a11a72ec292a760b3417c38abea6
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-07-02 22:32:11 +0600

    usermodel added

---
+++ models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose, { mongo } from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "User Name is Required"],
+      trim: true,
+      minLength: 2,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      required: [true, "Email address is Required"],
+      trim: true,
+      lowercase: true,
+      minLength: 2,
+      maxLength: 255,
+      match: [/\S+@\S+\.\S+/, "Please fill a valid email address"],
+    },
+    password: {
+      type: String,
+      required: [true, "User password is required"],
+      minLength: 6,
+    },
+  },
+  { timestamps: true }
+);
+
+const user = mongoose.model("User", userSchema);
+
+export default user;
