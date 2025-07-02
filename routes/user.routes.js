48b27d785bcd610c3e7719be86e0179ff3c29985
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-07-02 22:28:39 +0600

    API scaffolding

---
+++ routes/user.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const userRouter = Router();
+
+userRouter.get("/", (req, res) => res.send({ title: "GET all users" }));
+
+userRouter.get("/:id", (req, res) => res.send({ title: "GET user Details" }));
+
+userRouter.post("/", (req, res) => res.send({ title: "CREATE a user" }));
+
+userRouter.put("/", (req, res) => res.send({ title: "UPDATE user" }));
+
+userRouter.delete("/", (req, res) => res.send({ title: "DELETE  users" }));
