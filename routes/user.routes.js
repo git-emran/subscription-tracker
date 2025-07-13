7fd170d97409463a09a509a5b63c8ead4f7e224f
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-07-13 15:12:44 +0600

    authorization middleware added

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getUsers } from "../controllers/user.controller";
+import { getUser, getUsers } from "../controllers/user.controller";
+import authorize from "../middlewares/auth.middleware";
 
 const userRouter = Router();
 
 userRouter.get("/", getUsers);
 
-userRouter.get("/:id", (req, res) => res.send({ title: "GET user Details" }));
+userRouter.get("/:id", authorize, getUser);
 
 userRouter.post("/", (req, res) => res.send({ title: "CREATE a user" }));
 
